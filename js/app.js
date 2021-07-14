22edaa8326951bb977373e149149c686a8dfa283
Suketbb <suketbhola2002@gmail.com>
2021-07-14 18:50:34 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// const card = document.querySelectorAll('.card')
+
+// for(let c of card){
+//     c.addEventListener('mouseover',(event)=>{
+//         c.classList.add('applyShawdow')
+//         setTimeout(()=>{
+//         c.classList.remove('applyShawdow')
+//         },1000)
+//     })
+// }
